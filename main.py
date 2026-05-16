03f3488e7d80bed844210edd23c422b3e03d0ff2
Evan Shipley-courter <evanshipley-courter@Evans-MacBook-Pro.local>
2026-05-16 05:46:05 -0400

    Add multi-tabbed workspaces and settings button to GUI

--- main.py
+++ main.py
@@ -9,55 +9,58 @@ import threading
 
 class Api:
     def __init__(self):
-        self.fd = None
+        self.ptys = {} # tab_id -> fd
         self.window = None
 
     def set_window(self, window):
         self.window = window
 
-    def start_terminal(self, cwd, shell):
-        if self.fd is not None:
-            os.close(self.fd)
+    def start_terminal(self, tab_id, cwd, shell):
+        if tab_id in self.ptys:
+            os.close(self.ptys[tab_id])
             
-        pid, self.fd = pty.fork()
+        pid, fd = pty.fork()
         if pid == 0: # Child
-            os.chdir(cwd)
+            try:
+                os.chdir(cwd)
+            except Exception:
+                os.chdir("/")
             os.environ['TERM'] = 'xterm-256color'
             os.execv(shell, [shell])
         else: # Parent
+            self.ptys[tab_id] = fd
             # Set non-blocking
-            flags = fcntl.fcntl(self.fd, fcntl.F_GETFL)
-            fcntl.fcntl(self.fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+            flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+            fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
             
-            # Start reading thread
-            threading.Thread(target=self.read_from_pty, daemon=True).start()
+            # Start reading thread for this specific PTY
+            threading.Thread(target=self.read_from_pty, args=(tab_id, fd), daemon=True).start()
             return True
 
-    def read_from_pty(self):
+    def read_from_pty(self, tab_id, fd):
         while True:
-            r, _, _ = select.select([self.fd], [], [])
-            if self.fd in r:
+            if tab_id not in self.ptys: break
+            r, _, _ = select.select([fd], [], [], 0.1)
+            if fd in r:
                 try:
-                    data = os.read(self.fd, 1024).decode('utf-8', errors='replace')
+                    data = os.read(fd, 1024).decode('utf-8', errors='replace')
                     if data and self.window:
-                        self.window.evaluate_js(f"writeToTerminal({repr(data)})")
+                        self.window.evaluate_js(f"writeToTerminal('{tab_id}', {repr(data)})")
                 except OSError:
                     break
+        if tab_id in self.ptys:
+            del self.ptys[tab_id]
 
-    def write_to_pty(self, data):
-        if self.fd:
-            os.write(self.fd, data.encode('utf-8'))
+    def write_to_pty(self, tab_id, data):
+        fd = self.ptys.get(tab_id)
+        if fd:
+            os.write(fd, data.encode('utf-8'))
 
-    def resize_pty(self, cols, rows):
-        if self.fd:
+    def resize_pty(self, tab_id, cols, rows):
+        fd = self.ptys.get(tab_id)
+        if fd:
             winsize = struct.pack("HHHH", rows, cols, 0, 0)
-            fcntl.ioctl(self.fd, termios.TIOCSWINSZ, winsize)
-
-    def get_directories(self, path):
-        try:
-            return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
-        except Exception:
-            return []
+            fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
 
 if __name__ == '__main__':
     api = Api()
@@ -65,8 +68,8 @@ if __name__ == '__main__':
         'Jarvis Term - Warp Style',
         'index.html',
         js_api=api,
-        width=1000,
-        height=700,
+        width=1100,
+        height=750,
         background_color='#1E1E1E'
     )
     api.set_window(window)
